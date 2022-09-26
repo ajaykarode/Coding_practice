6be27420c9ed697afba88f00b609e7a75245fccf
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2022-09-26 15:44:42 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (99.80%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -4,13 +4,13 @@ public:
         int n = matrix.size();
         for(int i = 0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                swap(matrix[i][j],matrix[j][i]);       
+                swap(matrix[i][j],matrix[j][i]);     //transpose of matrix     
             }
         } 
         for(int i = 0;i<n;i++){
             int start = 0,end = n-1;
             while(start<end){
-                swap(matrix[i][start],matrix[i][end]);
+                swap(matrix[i][start],matrix[i][end]); //reverse the matrix by row
                 start++;
                 end--;
             
