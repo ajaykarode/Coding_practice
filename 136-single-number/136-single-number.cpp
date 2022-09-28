e6a4e16350c64ed0cede53d577d18ad2c207e12a
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2022-09-28 23:27:58 +0530

    Time: 59 ms (16.78%), Space: 17 MB (53.54%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        // sort(nums.begin(),nums.end());
         int n = nums.size();
-        unordered_map<int,int>ans;
-        for(auto i:nums){
-            ans[i]++;
-        }
-        for(auto j:ans)
-            if(j.second == 1)
-                return j.first;
-        return -1;
+        // unordered_map<int,int>ans;
+        // for(auto i:nums){
+        //     ans[i]++;
+        // }
+        // for(auto j:ans)
+        //     if(j.second == 1)
+        //         return j.first;
+        // return -1;
+        
+        
+        
+        // OR
+        
+        int ans = 0;
+        for(auto i: nums)
+            ans^= i;
+        return ans;
         
     }
 };
