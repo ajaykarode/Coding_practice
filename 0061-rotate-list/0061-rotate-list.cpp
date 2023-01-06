52fa721470e9a5dacc76ac62c0b58bd798ddefb6
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-01-06 17:41:31 +0530

    Time: 3 ms (98.65%), Space: 11.8 MB (59.36%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -6,31 +6,30 @@
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };       
+ * };
  */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        ListNode* p = head;
-        if(head == NULL || head->next == NULL || k == 0)
-            return head;
+    if(head == NULL||head->next == NULL||k == 0) return head;
+    ListNode* temp = head;
+    int length = 1;
+    while(temp-> next != NULL){
+        length++;
+        temp = temp->next;
+    }
+    temp->next = head;
+    
+    k = k%length;
+    int end = length - k;
+    while(end--)
+        temp = temp->next;
+    
+    head = temp->next;
+    temp->next = NULL;
+    return head;
         
-        ListNode* temp = head;
-        int count = 1;
-        while(temp->next != NULL){
-            count++;
-            temp = temp->next;
-        }
-        temp->next = head;
-        k = k%count;
-        int res = count-k;
-        while(res--)
-            temp = temp->next;
-        
-        head = temp->next;
-        temp->next = NULL;
-        return head;
-            
         
     }
 };
+
