bd6bfa86f78282937f2584ec354a6d1b8f129771
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-01-06 19:27:45 +0530

    Time: 19 ms (23.67%), Space: 11.3 MB (30.14%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,41 +17,53 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        // step-1
-        Node* node = head;
-        // Node* front = head;
-        while(node){
-            Node* temp = node->next;
-            node->next = new Node(node->val);
-            node->next->next = temp;
-            node = temp;
-            
+        Node* iter = head;
+        Node* front = head;
+        while(iter){
+            Node* front = iter->next;
+            Node* copy = new Node(iter->val);
+            iter->next = copy;
+            copy->next = front;
+            iter = front;
+        }
+        iter = head;
+        while(iter){
+            if(iter->random)
+                iter->next->random = iter->random->next;
+            iter = iter->next->next;
         }
         
+        // iter = head;
         
-        // step-2
-        node = head;
-        while(node){
-            if(node->random)
-                node->next->random = node->random->next;
-            
-            node = node->next->next;
-        }
-        
-        
-        // step-3
-        // node = head;
         Node* ans = new Node(0);
+        
         Node* helper = ans;
         while(head){
-        
+            
             helper->next = head->next;
             helper = helper->next;
             
             head->next = head->next->next;
             head = head->next;
+            
         }
         return ans->next;
         
+        
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
