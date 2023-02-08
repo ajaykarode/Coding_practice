0034c571b3af532e4f90ffbf79b0004487f6e913
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-02-08 22:49:55 +0530

    Time: 6 ms (40.53%), Space: 7.6 MB (92.54%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    void func(int index,vector<int> &nums,vector<vector<int>> &ans){
+        if(index == nums.size()){
+            ans.push_back(nums);
+            return ;
+        }
+        for(int i = index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            func(index+1,nums,ans);
+            swap(nums[index],nums[i]);
+        }
+    }
+    
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        func(0,nums,ans);
+        return ans;
+    }
+};
+
+
+
