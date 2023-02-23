9205caf2d8fd191e4b125f93b0a7fd8c006b0a38
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-02-24 00:40:41 +0530

    Time: 41 ms (11.00%), Space: 27.5 MB (6.91%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(auto x: nums)
+            m[x]++;
+        
+        for(auto y: m)
+            if(y.second ==1)
+                return y.first;
+            
+        
+        return -1;
+    }
+};
