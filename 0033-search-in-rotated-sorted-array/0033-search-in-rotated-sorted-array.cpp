6f30b633de8dc2da291eab15e471d8a384bd004a
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-02-25 17:41:39 +0530

    Time: 7 ms (38.99%), Space: 11.1 MB (43.56%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int index = -1;
+        // int index = -1;
         for(int i = 0;i<nums.size();i++){
             if(nums[i] == target)
-                index = i;
+                return i;
             // index = -1;
             
         }
-        return index;
+        return -1;
         
     }
 };
