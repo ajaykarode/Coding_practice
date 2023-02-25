a4f10f2ef15fcf03d950f0c2f68487079f20c9d9
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-02-25 20:10:57 +0530

    Time: 40 ms (72.24%), Space: 89.3 MB (75.10%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,28 +1,88 @@
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         vector<int>v;
+//         int i,j;
+//         for(auto i: nums1)
+//             v.push_back(i);
+        
+//         for(auto j: nums2)
+//             v.push_back(j);
+        
+//         sort(v.begin(),v.end());
+        
+//         int n = v.size();
+        
+//         double res = 0;
+//         if(n % 2 == 0){
+//             // res = v[n/2];
+//             res = (v[n/2] + v[n/2-1])/2.0;
+//         }
+//         else
+//             // res = (v[n/2] + v[n/2-1])/2.00000;
+//             res = v[n/2];
+//         // return n%2?v[n/2]:(v[n/2-1]+v[n/2])/2.0;
+//         return res;
+//     }
+// };
+
+
+
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>v;
-        int i,j;
-        for(auto i: nums1)
-            v.push_back(i);
+    if(nums2.size() < nums1.size()) return findMedianSortedArrays(nums2,nums1);
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int low = 0;
+        int high = n1;
         
-        for(auto j: nums2)
-            v.push_back(j);
-        
-        sort(v.begin(),v.end());
-        
-        int n = v.size();
-        
-        double res = 0;
-        if(n % 2 == 0){
-            // res = v[n/2];
-            res = (v[n/2] + v[n/2-1])/2.0;
+        while( low <= high){
+            
+            int cut1 = (low + high) >> 1;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+            
+            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
+            
+            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+            
+            if(left1 <= right2 && left2 <= right1){
+                if((n1 + n2) % 2 == 0)
+                    return (max(left1,left2) + min(right1,right2)) / 2.0;
+                
+                else
+                    return max(left1,left2);
+            }
+            else if(left1 > right2)
+                high = cut1-1;
+            
+            else
+                low = cut1+1;
         }
-        else
-            // res = (v[n/2] + v[n/2-1])/2.00000;
-            res = v[n/2];
-        // return n%2?v[n/2]:(v[n/2-1]+v[n/2])/2.0;
-        return res;
+        return 0.0;
+
     }
 };
-// n%2?v[n/2]:(v[n/2-1]+v[n/2])/2.0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
