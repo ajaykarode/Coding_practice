78211a118b0e05e08b29f05b07d391d13b898cc0
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-02-25 18:09:13 +0530

    Time: 7 ms (38.99%), Space: 11.2 MB (43.56%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,14 +1,43 @@
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         for(int i = 0;i<nums.size();i++){
+//             if(nums[i] == target)
+//                 return i; }
+//         return -1;    
+//     }};
+
+
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        // int index = -1;
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i] == target)
-                return i;
-            // index = -1;
+        
+        int low = 0;
+        int high = nums.size() -1;
+        
+        while(low <= high){
+            int mid = (low + high) >> 1;
             
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && nums[mid] >=target)
+                    high = mid -1;
+                else
+                    low = mid + 1;
+            }
+            else
+                if(nums[mid] <= target && nums[high] >= target)
+                    low = mid + 1;
+                
+                else
+                    high = mid - 1;
+                    
         }
         return -1;
+        
         
     }
 };
