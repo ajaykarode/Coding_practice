0ad498cc8af93ff7c84d405a2c0ba2b6760f0452
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-03-26 00:19:29 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (78.11%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,35 +1,96 @@
+// class MyStack {
+// public:
+// //     MyStack() {
+        
+// //     }
+    
+//     queue<int>q1;
+//     queue<int>q2
+//     void push(int x) {
+//         q.push(x);
+//         for(int i = 0;i<q.size()-1;i++){
+//             q.push(q.front());
+//             q.pop();
+//         }
+//     }
+    
+//     int pop() {
+//         int temp = q.front();
+//         q.pop();
+//         return temp;
+        
+//     }
+    
+//     int top() {
+//         return q.front();
+        
+//     }
+    
+//     bool empty() {
+//         return q.empty();
+//     }
+// };
+
+
 class MyStack {
 public:
 //     MyStack() {
         
 //     }
     
-    queue<int>q;
+    queue<int>q1;
+    queue<int>q2;
+        
+        
     void push(int x) {
-        q.push(x);
-        for(int i = 0;i<q.size()-1;i++){
-            q.push(q.front());
-            q.pop();
-        }
+        q1.push(x);
+        
     }
     
     int pop() {
-        int temp = q.front();
-        q.pop();
-        return temp;
+        int n = q1.size();
+        while(n > 1){
+            int temp = q1.front();
+            q2.push(temp);
+            q1.pop();
+            n--;
+        }
+        int x = q1.front();
+        q1.pop();
+        
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        return x;
+
         
     }
     
     int top() {
-        return q.front();
+        int n = q1.size();
+        while(n > 1){
+            int temp = q1.front();
+            q2.push(temp);
+            q1.pop();
+            n--;
+        }
+        int x = q1.front();
+        q1.pop();
+        
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        q1.push(x);
+        return x;
         
     }
     
     bool empty() {
-        return q.empty();
+        return q1.empty();
     }
 };
-
 /**
  * Your MyStack object will be instantiated and called as such:
  * MyStack* obj = new MyStack();
