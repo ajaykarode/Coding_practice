6d66230b73cd4b0444d2e5c6a5de7e4e543b0d4c
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-05-18 14:24:09 +0530

    Time: 3 ms (36.10%), Space: 6 MB (88.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        int N = n+m-2;
-        int r = m-1;
+        int N = m+n-2;
+        int r = n-1;
         double ans = 1;
         for(int i = 1;i<=r;i++){
-            ans = ans* (N-r+i)/i;
+            ans = ans * (N - r + i) / i;
+            // ans = ans/i+1;
         }
-        return int(ans);
-        
-        
+        return (int)ans;
+        // return 0;
     }
 };
