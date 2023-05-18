db887f94ca154a4b7ce2304d5d49df95778bac65
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-05-19 01:38:04 +0530

    Time: 31 ms (86.60%), Space: 71.5 MB (45.74%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,26 +11,53 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* p = new ListNode();
-        ListNode* q  = p;
+        ListNode* dummy = new ListNode();
+        ListNode* temp = dummy;
         int carry = 0;
+        
         while(l1 != NULL || l2!= NULL || carry){
             int sum = 0;
-            if(l1 != NULL){
-            sum += l1->val;
-            l1 = l1->next;
+            if(l1!= NULL){
+                sum+= l1->val;
+                l1 = l1->next;
+            }
+            if(l2!= NULL){
+                sum+= l2->val;
+                l2 = l2->next;
+            }            
+            sum += carry;
+            carry = sum/10;
+            ListNode* h = new ListNode(sum%10);
+            temp->next = h;
+            temp = temp->next;            
         }
-        if(l2 != NULL){
-            sum += l2->val;
-            l2 = l2->next;
-        }
-        sum += carry;
-        carry = sum/10; 
-        ListNode* hello = new ListNode(sum%10);
-        q->next = hello;
-        q = q->next;  
-            
-        }
-        return p->next;
+        return dummy->next;       
     }
 };
+
+// class Solution {
+// public:
+//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+//         ListNode* p = new ListNode();
+//         ListNode* q  = p;
+//         int carry = 0;
+//         while(l1 != NULL || l2!= NULL || carry){
+//             int sum = 0;
+//             if(l1 != NULL){
+//             sum += l1->val;
+//             l1 = l1->next;
+//         }
+//         if(l2 != NULL){
+//             sum += l2->val;
+//             l2 = l2->next;
+//         }
+//         sum += carry;
+//         carry = sum/10; 
+//         ListNode* hello = new ListNode(sum%10);
+//         q->next = hello;
+//         q = q->next;  
+            
+//         }
+//         return p->next;
+//     }
+// };
