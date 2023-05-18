1b75bacd919efadec671e2050307febb162544bb
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-05-18 19:41:03 +0530

    Time: 12 ms (32.65%), Space: 14.7 MB (77.73%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,40 +11,33 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
-        if(list2 == NULL) return list1;
-        if(list1 == NULL) return list2;
-        
-        ListNode *head = NULL;
-        if(list1->val < list2->val){
-            head = list1;
-            list1 = list1->next;
-        }
-        else{
-            head = list2;
-            list2 = list2->next;
-        }
-        
-        ListNode *p = head;
-        while(list1 && list2){
-            if(list1->val < list2->val){
-                p->next = list1;
-                list1 = list1->next;
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list2;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp3 = dummy;
+    
+        while(temp1 != NULL && temp2 != NULL){
+            if(temp1->val < temp2->val){
+                temp3->next = temp1;
+                temp1 = temp1->next;        
             }
             else{
-                p->next = list2;
-                list2 = list2->next;
+                temp3->next = temp2;
+                temp2 = temp2->next;
             }
-            p = p->next;
+            temp3 = temp3->next;
+        }
+        while(temp1!= NULL){
+            temp3->next = temp1;
+            temp1 = temp1->next;
+            temp3 = temp3->next;
+        }
+        while(temp2!= NULL){
+            temp3->next = temp2;
+            temp2 = temp2->next;
+            temp3 = temp3->next;
+        }
+        return dummy->next;
             
         }
-        if(list1){
-            p->next = list1;
-        }
-        else{
-            p->next = list2;
-        }
-        return head;
-        
-    }
 };
