2bdef6c39cbb45ed0319e6d76e70dda4cb527efe
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-05-20 02:25:48 +0530

    Time: 11 ms (46.79%), Space: 11.1 MB (91.44%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,37 +17,90 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+//         Node* iter = head;
+//         Node* front = head;
+//         while(iter){
+//             Node* front = iter->next;
+//             Node* copy = new Node(iter->val);
+//             iter->next = copy;
+//             copy->next = front;
+//             iter = front;
+//         }
+//         iter = head;
+//         while(iter){
+//             if(iter->random)
+//                 iter->next->random = iter->random->next;
+//             iter = iter->next->next;
+//         }
+        
+//         // iter = head;
+        
+//         Node* ans = new Node(0);
+        
+//         Node* helper = ans;
+//         while(head){
+            
+//             helper->next = head->next;
+//             helper = helper->next;
+            
+//             head->next = head->next->next;
+//             head = head->next;
+            
+//         }
+//         return ans->next;
+       
+        
+        
+        
+                   // PART-1 - CREATING A COPY NODE
+        
         Node* iter = head;
         Node* front = head;
-        while(iter){
-            Node* front = iter->next;
+        
+        while(iter!= NULL){
+            front = iter->next;
             Node* copy = new Node(iter->val);
             iter->next = copy;
             copy->next = front;
             iter = front;
         }
+        
+        
+                // PART-2 - POINTING RANDOM POINTER
         iter = head;
-        while(iter){
-            if(iter->random)
-                iter->next->random = iter->random->next;
+        while(iter!= NULL){
+            if(iter->random){
+                iter->next->random = iter->random->next;   
+            }
             iter = iter->next->next;
         }
         
-        // iter = head;
         
+                // PART-3 - SEPARATING COPY NODE AND ORIGINAL NODE
+        
+        iter= head;
         Node* ans = new Node(0);
+        Node* point = ans;
         
-        Node* helper = ans;
-        while(head){
+        while(iter!= NULL){
+            point->next = iter->next;
+            point = point->next;
             
-            helper->next = head->next;
-            helper = helper->next;
-            
-            head->next = head->next->next;
-            head = head->next;
+            iter->next = iter->next->next;
+            iter = iter->next;
             
         }
         return ans->next;
+        
+        // helper->next = head->next;
+//             helper = helper->next;
+            
+//             head->next = head->next->next;
+//             head = head->next;
+            
+//         }
+//         return ans->next;
+        
         
         
     }
