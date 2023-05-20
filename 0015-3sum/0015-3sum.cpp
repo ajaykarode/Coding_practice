5129ba40a05ab860a57c62fc1e549f6c6a59643a
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-05-20 14:29:36 +0530

    Time: 185 ms (63.72%), Space: 29.8 MB (47.09%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,40 +1,59 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>sol;
+    //     vector<vector<int>>sol;
+    //     sort(nums.begin(),nums.end());
+    //     for(int i = 0;i<nums.size()-2;i++){
+    //         if(i == 0 || i > 0 && nums[i] != nums[i-1]){
+    //             int lo = i+1, high = nums.size()-1,sum = 0-nums[i];
+    //             while(lo < high){                    
+    //                 if(nums[lo] + nums[high] == sum){                        
+    //                     vector<int> ans;
+    //                     ans.push_back(nums[i]);
+    //                     ans.push_back(nums[lo]);
+    //                     ans.push_back(nums[high]);
+    //                     sol.push_back(ans);                        
+    //                     while(lo < high && nums[lo] == nums[lo+1]) lo++;
+    //                     while(lo < high && nums[high] == nums[high-1]) high--;                
+    //                     lo++, high--; }
+    //                 else if(nums[lo] + nums[high] < sum)lo++;            
+    //                 else
+    //                     high--;
+    //             }
+    //         } 
+    // }
+       // return sol;   
+        
+        
+        
+    
+        vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
         for(int i = 0;i<nums.size()-2;i++){
-            
-            if(i == 0 || i > 0 && nums[i] != nums[i-1]){
-                
-                int lo = i+1, high = nums.size()-1,sum = 0-nums[i];
-                
-                while(lo < high){
-                    
-                    if(nums[lo] + nums[high] == sum){
-                        
-                        vector<int> ans;
-                        ans.push_back(nums[i]);
-                        ans.push_back(nums[lo]);
-                        ans.push_back(nums[high]);
-                        sol.push_back(ans);
-                        
-                        while(lo < high && nums[lo] == nums[lo+1]) lo++;
-                        while(lo < high && nums[high] == nums[high-1]) high--;
-                        
-                        lo++, high--;
-                    }
-                    else if(nums[lo] + nums[high] < sum)
-                        lo++;
-            
-                    else
-                        high--;
+            if(i == 0 || i>0 && nums[i] != nums[i-1]){
+            int lo = i+1,high = nums.size()-1,sum = 0 - nums[i];
+            while(lo < high){
+                if(nums[lo] + nums[high] == sum){
+                    vector<int>temp;
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[lo]);
+                    temp.push_back(nums[high]);   
+                    ans.push_back(temp);                    
+                    while(lo < high && nums[lo] == nums[lo+1]) lo++;
+                    while(lo < high && nums[high] == nums[high-1]) high--  ;                 
+                    lo++;
+                    high--;
+                }
+                else if(nums[lo] + nums[high] < sum){
+                    lo++;
+                }
+                else{
+                    high--;
                 }
             }
-            
-            
         }
-        return sol;
+        }
         
+        return ans;
     }
 };
