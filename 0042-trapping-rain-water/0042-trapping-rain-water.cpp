ff6390bff408cad3590b1ca43cd392e4cf582eb1
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-05-21 00:11:42 +0530

    Time: 23 ms (21.35%), Space: 19.8 MB (81.94%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,25 +1,79 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-        int left = 0,right = n-1;
-        int ans = 0;
-        int maxleft = 0,maxright = 0;
         
-        while(left<=right){
-            if(height[left]<= height[right]){
-                if(height[left]>= maxleft) maxleft = height[left];
-                else ans+= maxleft - height[left];
-            
-            left++;
+        int n = height.size();
+        int left = 0;
+        int right = n-1;
+        int leftmax = 0;
+        int rightmax = 0;
+        int count = 0;
+        while(left <= right){
+            if(height[left] <= height[right]){
+                if(height[left] > leftmax)  leftmax = height[left];
+                else count+= leftmax - height[left];        
+                left++;
             }
             else{
-                if(height[right]>= maxright) maxright = height[right];
-                else ans+= maxright - height[right];
+                if(height[right] > rightmax) rightmax = height[right];
+                else count+= rightmax - height[right];
                 
                 right--;
             }
         }
-        return ans;
+        return count;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int n = height.size();
+//         int left = 0,right = n-1;
+//         int ans = 0;
+//         int maxleft = 0,maxright = 0;
+        
+//         while(left<=right){
+//             if(height[left]<= height[right]){
+//                 if(height[left]>= maxleft) maxleft = height[left];
+//                 else ans+= maxleft - height[left];
+            
+//             left++;
+//             }
+//             else{
+//                 if(height[right]>= maxright) maxright = height[right];
+//                 else ans+= maxright - height[right];
+                
+//                 right--;
+//             }
+//         }
+//         return ans;
