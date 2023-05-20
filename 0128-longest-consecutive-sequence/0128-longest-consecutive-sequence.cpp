caac79400cbdb686003af1679b26e3cf2f6d55b7
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-05-20 17:41:14 +0530

    Time: 93 ms (79.46%), Space: 43.4 MB (77.78%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,50 +1,43 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+//         if(nums.size() == 0){
+//             return 0;
+//         }
+//         int count = 1;
+//         int ans = 1;
+//         sort(nums.begin(),nums.end()); 
+//         int prev = nums[0];
+//         for(int i = 1;i<nums.size();i++){
+//             if(nums[i] == prev + 1) 
+//                 {
+//                     count++;
+//                 }
+//             else if(nums[i] != prev){
+//                 count = 1;
+//             }
+//             prev = nums[i];
+//             ans = max(ans,count);
+            
+//         }
+//         return ans;
         if(nums.size() == 0){
             return 0;
         }
         int count = 1;
-        int ans = 1;
-        sort(nums.begin(),nums.end()); 
+        int maxi = 1;
+        sort(nums.begin(),nums.end());
         int prev = nums[0];
         for(int i = 1;i<nums.size();i++){
-            if(nums[i] == prev + 1) 
-                {
-                    count++;
-                }
+            if(nums[i] == prev+1){
+                count++;
+            }
             else if(nums[i] != prev){
                 count = 1;
             }
             prev = nums[i];
-            ans = max(ans,count);
-            
+            maxi = max(maxi,count);
         }
-        return ans;
+        return maxi;
     }
 };
-
-
-// class Solution {
-// public:
-//     int longestConsecutive(vector<int>& nums) {
-//         if(nums.size() == 0 ){
-//             return 0;
-//         }    
-//         sort(nums.begin(),nums.end());        
-//         int ans = 1;
-//         int prev = nums[0];
-//         int cur = 1;      
-//         for(int i = 1;i < nums.size();i++){
-//             if(nums[i] == prev+1){
-//                 cur++;
-//             }
-//             else if(nums[i] != prev){
-//                 cur = 1;
-//             }
-//             prev = nums[i];
-//             ans = max(ans, cur);
-//         }
-//         return ans;
-//     }
-// };
