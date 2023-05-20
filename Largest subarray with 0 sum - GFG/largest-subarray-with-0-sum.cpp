9c72c9699d5ca5f4a229185ff3fd00a1272167bb
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-05-20 23:10:18 +0530

    Added solution - LeetHub

--- Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.cpp
@@ -8,6 +8,34 @@ using namespace std;
 // } Driver Code Ends
 /*You are required to complete this function*/
 
+// class Solution{
+//     public:
+//     int maxLen(vector<int>&A, int n)
+//     {   
+//         // Your code here
+//         int left = 0,right = 0;
+//         int k = 0;
+//         int maxlen = 0;
+//         int sum = A[0];
+//         while(right < n){
+//             while(left <= right && sum > k){
+//                 sum -= A[left];
+//                 left++;
+//             }
+            
+//             if(sum == k){
+//                 maxlen = max(maxlen, right - left + 1);
+//             }
+             
+//             right++;
+//             if(right < n) sum+= A[right];
+//         }
+//         return maxlen;
+//     }
+// };
+
+/*You are required to complete this function*/
+
 class Solution{
     public:
     int maxLen(vector<int>&A, int n)
