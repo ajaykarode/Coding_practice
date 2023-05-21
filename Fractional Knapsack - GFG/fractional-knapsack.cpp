901ec95266a2dca8d9f99928d112cfdfd407a4a7
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-05-21 13:42:02 +0530

    Added solution - LeetHub

--- Fractional Knapsack - GFG/fractional-knapsack.cpp
+++ Fractional Knapsack - GFG/fractional-knapsack.cpp
@@ -22,53 +22,65 @@ class Solution
 {
     public:
     //Function to get the maximum total value in the knapsack.
+    // static bool cmp(Item a, Item b){
+    //     double i1 = (double)a.value/(double)a.weight;
+    //     double i2 = (double)b.value/(double)b.weight;
+    //     return i1 > i2;
+    // }
+    
+    
     static bool cmp(Item a, Item b){
-        double i1 = (double)a.value/(double)a.weight;
-        double i2 = (double)b.value/(double)b.weight;
-        return i1 > i2;
+        double num1 = (double)a.value/(double)a.weight;
+        double num2 = (double)b.value/(double)b.weight;
+        return num1 > num2;
     }
     
     double fractionalKnapsack(int W, Item arr[], int n)
     {
         // Your code here
+        // sort(arr,arr+n,cmp);
+        // double profit = 0.0;
+        // int cweight = 0;
+        // for(int i = 0;i<n;i++){
+        //     if(cweight + arr[i].weight <= W){
+        //         cweight += arr[i].weight;
+        //         profit += arr[i].value;
+        //     }
+        //     else{
+        //         int remain = W - cweight;
+        //         profit += ((double)arr[i].value/arr[i].weight) * remain;
+        //         break;
+        //     }
+        // }
         sort(arr,arr+n,cmp);
-        double profit = 0.0;
-        int cweight = 0;
         
+        int currweight = 0;
+        double profit = 0.0;
         for(int i = 0;i<n;i++){
-            if(cweight + arr[i].weight <= W){
-                cweight += arr[i].weight;
-                profit += arr[i].value;
+            if(currweight + arr[i].weight <= W){
+                currweight+= arr[i].weight;
+                profit+= arr[i].value;
             }
             else{
-                int remain = W - cweight;
-                profit += ((double)arr[i].value/arr[i].weight) * remain;
+                int remain = W- currweight;
+                profit+= ((double)arr[i].value/arr[i].weight) * remain;
                 break;
             }
         }
+        return profit;
         
     }
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
 };
+
+
+
+
+
+
+
+
 
 
 //{ Driver Code Starts.
