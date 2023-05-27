2a613d3f003ecc98aa529ed47b4e08380a3df3be
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-05-27 22:56:32 +0530

    Time: 8 ms (58.87%), Space: 10.6 MB (89.79%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     void combi(int ind,int target,vector<int> &candidates,vector<vector<int>>&ans,vector<int>&ds){
-        
-        
-        
+     
     
-    
-    if(ind == candidates.size()){
-        if(target == 0){
-            ans.push_back(ds);
+    // if(ind == candidates.size()){
+    //     if(target == 0){
+    //         ans.push_back(ds);
+    //     }
+    //     return;
+    // }
+    // if(candidates[ind] <= target){
+    //     ds.push_back(candidates[ind]);
+    //     combi(ind,target - candidates[ind],candidates,ans,ds);
+    //     ds.pop_back();
+    // }
+    // combi(ind+1,target,candidates,ans,ds);
+        
+        
+        if(ind == candidates.size()){
+            if(target == 0){
+                ans.push_back(ds);
+            }
+            return ;
         }
-        return;
-    }
-    if(candidates[ind] <= target){
-        ds.push_back(candidates[ind]);
-        combi(ind,target - candidates[ind],candidates,ans,ds);
-        ds.pop_back();
-    }
-    combi(ind+1,target,candidates,ans,ds);
+
+            if(candidates[ind] <= target){
+            ds.push_back(candidates[ind]);
+            combi(ind,target - candidates[ind],candidates,ans,ds);
+            ds.pop_back();
+        }
+        combi(ind+1,target,candidates,ans,ds);
     
     
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         
