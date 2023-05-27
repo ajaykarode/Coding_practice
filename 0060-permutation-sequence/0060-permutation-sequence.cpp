ad6eac322c0cee724c7faa99d30e0e2ec438f52e
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-05-28 00:38:16 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (19.14%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,24 +1,44 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        vector<int>numbers;
+//         vector<int>numbers;
+//         int fact = 1;
+//         for(int i = 1;i<n;i++){
+//             fact = fact * i;
+//             numbers.push_back(i);
+//         }
+//         numbers.push_back(n);
+        
+//         string ans = "";
+//         k = k-1;
+//         while(true){
+//             ans = ans + to_string(numbers[k/fact]);
+//             numbers.erase(numbers.begin() + k/fact);
+//             if(numbers.size() == 0){
+//                 break;
+//             }
+//             k = k % fact;
+//             fact = fact / numbers.size();
+//         }
+//         return ans;
+        
         int fact = 1;
-        for(int i = 1;i<n;i++){
+        vector<int>numbers;
+        for(int i  =1;i<n;i++){
             fact = fact * i;
             numbers.push_back(i);
         }
         numbers.push_back(n);
         
-        string ans = "";
+        string ans ="";
         k = k-1;
         while(true){
             ans = ans + to_string(numbers[k/fact]);
             numbers.erase(numbers.begin() + k/fact);
-            if(numbers.size() == 0){
-                break;
-            }
-            k = k % fact;
-            fact = fact / numbers.size();
+            if(numbers.size() == 0) break;
+            
+            k = k% fact;
+            fact = fact/numbers.size();
         }
         return ans;
         
