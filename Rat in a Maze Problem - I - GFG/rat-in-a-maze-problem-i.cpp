1f0f8eaab0fd0c6d9dc2a8115f6295b4e21d5d73
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-05-28 18:10:57 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -10,36 +10,67 @@ using namespace std;
 
 class Solution{
     
-    void solve(int i,int j, vector<vector<int>> &a,int n,vector<string> &ans,string move,vector<vector<int>> &vis){
-        if(i ==n-1 && j ==n-1){
+    // void solve(int i,int j, vector<vector<int>> &a,int n,vector<string> &ans,string move,vector<vector<int>> &vis){
+        // if(i ==n-1 && j ==n-1){
+        //     ans.push_back(move);
+        //     return;
+        // }
+        // //down
+        
+        // if(i+1<n && !vis[i+1][j] && a[i+1][j] == 1){
+        //     vis[i][j] = 1;
+        //     solve(i+1,j,a,n,ans,move + 'D',vis);
+        //     vis[i][j] = 0;
+        // }
+        
+        // //left
+        // if(j-1 >= 0 && !vis[i][j-1] && a[i][j-1] == 1){
+        //     vis[i][j] = 1;
+        //     solve(i,j-1,a,n,ans,move + 'L',vis);
+        //     vis[i][j] = 0;
+        // }
+        // //right
+        // if(j+1 < n && !vis[i][j+1] && a[i][j+1] == 1){
+        //     vis[i][j] = 1;
+        //     solve(i,j+1,a,n,ans,move + 'R',vis);
+        //     vis[i][j] = 0;
+        // }
+        
+        // //up
+        // if(i-1 >= 0 && !vis[i-1][j] && a[i-1][j] == 1){
+        //     vis[i][j] = 1;
+        //     solve(i-1,j,a,n,ans,move + 'U',vis);
+        //     vis[i][j] = 0;
+        // }
+        
+        
+    // }
+    void solve(int i,int j,vector<vector<int>>&a,int n,vector<string>&ans,string move,vector<vector<int>>&vis){
+        
+        if(i == n-1 && j== n-1){
             ans.push_back(move);
-            return;
-        }
-        //down
-        
-        if(i+1<n && !vis[i+1][j] && a[i+1][j] == 1){
-            vis[i][j] = 1;
-            solve(i+1,j,a,n,ans,move + 'D',vis);
-            vis[i][j] = 0;
+            return ;
         }
         
-        //left
-        if(j-1 >= 0 && !vis[i][j-1] && a[i][j-1] == 1){
+        if(i+1 < n && !vis[i+1][j] && a[i+1][j] == 1){
             vis[i][j] = 1;
-            solve(i,j-1,a,n,ans,move + 'L',vis);
+            solve(i+1,j,a,n,ans,move+ 'D',vis);
+            vis[i][j] = 0;
+            
+        }
+        if(j-1 >=0 && !vis[i][j-1] && a[i][j-1] == 1){
+            vis[i][j] = 1;
+            solve(i,j-1,a,n,ans,move+'L',vis);
             vis[i][j] = 0;
         }
-        //right
         if(j+1 < n && !vis[i][j+1] && a[i][j+1] == 1){
             vis[i][j] = 1;
-            solve(i,j+1,a,n,ans,move + 'R',vis);
+            solve(i,j+1,a,n,ans,move+'R',vis);
             vis[i][j] = 0;
         }
-        
-        //up
-        if(i-1 >= 0 && !vis[i-1][j] && a[i-1][j] == 1){
+        if(i-1 >= 0  && !vis[i-1][j] && a[i-1][j] == 1){
             vis[i][j] = 1;
-            solve(i-1,j,a,n,ans,move + 'U',vis);
+            solve(i-1,j, a,n,ans,move+'U',vis);
             vis[i][j] = 0;
         }
     }
