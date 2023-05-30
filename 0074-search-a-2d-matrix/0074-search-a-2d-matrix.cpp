151c39b291c2bb5e10f01f1e71a7e6ac55237c81
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-05-31 02:27:22 +0530

    Time: 3 ms (85.24%), Space: 9.4 MB (95.11%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,23 +2,43 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        if(!matrix.size()) return false;
-        int m = matrix.size();
-        int n = matrix[0].size();  
-        int lo = 0;
-        int high  = (m*n) - 1;
+//         if(!matrix.size()) return false;
+//         int m = matrix.size();
+//         int n = matrix[0].size();  
+//         int lo = 0;
+//         int high  = (m*n) - 1;
         
-        while(lo<=high){
-            int mid = (lo + (high - lo) / 2);
-            if(matrix[mid/n][mid%n] == target){
+//         while(lo<=high){
+//             int mid = (lo + (high - lo) / 2);
+//             if(matrix[mid/n][mid%n] == target){
+//                 return true;
+//             }
+//             if(matrix[mid/n][mid%n] < target){
+//                 lo = mid+1;
+//             }
+//             else{
+//                 high = mid - 1;
+//             }         
+            
+//         }
+//         return false;
+        
+        if(matrix.size() == 0) return 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int lo = 0;
+        int high = (m*n) - 1;
+        while(lo <= high){
+            int mid = (lo + (high - lo) /2);
+            if(matrix[mid/m][mid%m] == target){
                 return true;
             }
-            if(matrix[mid/n][mid%n] < target){
+            if(matrix[mid/m][mid%m] < target){
                 lo = mid+1;
             }
             else{
-                high = mid - 1;
-            }         
+                high = mid-1;
+            }
             
         }
         return false;
