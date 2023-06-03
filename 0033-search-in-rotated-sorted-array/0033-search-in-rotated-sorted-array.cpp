4785d34b814090a000a023f7f810032eaaadd237
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-06-04 01:41:39 +0530

    Time: 5 ms (47.35%), Space: 11.1 MB (24.44%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,43 +1,43 @@
-// class Solution {
-// public:
-//     int search(vector<int>& nums, int target) {
-//         for(int i = 0;i<nums.size();i++){
-//             if(nums[i] == target)
-//                 return i; }
-//         return -1;    
-//     }};
-
-
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] == target)
+                return i; }
+        return -1;    
+    }};
+
+
+
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
         
-        int low = 0;
-        int high = nums.size() -1;
+//         int low = 0;
+//         int high = nums.size() -1;
         
-        while(low <= high){
-            int mid = (low + high) >> 1;
+//         while(low <= high){
+//             int mid = (low + high) >> 1;
             
-            if(nums[mid] == target){
-                return mid;
-            }
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && nums[mid] >=target)
-                    high = mid -1;
-                else
-                    low = mid + 1;
-            }
-            else
-                if(nums[mid] <= target && nums[high] >= target)
-                    low = mid + 1;
+//             if(nums[mid] == target){
+//                 return mid;
+//             }
+//             if(nums[low] <= nums[mid]){
+//                 if(nums[low] <= target && nums[mid] >=target)
+//                     high = mid -1;
+//                 else
+//                     low = mid + 1;
+//             }
+//             else
+//                 if(nums[mid] <= target && nums[high] >= target)
+//                     low = mid + 1;
                 
-                else
-                    high = mid - 1;
+//                 else
+//                     high = mid - 1;
                     
-        }
-        return -1;
+//         }
+//         return -1;
         
         
-    }
-};
+//     }
+// };
