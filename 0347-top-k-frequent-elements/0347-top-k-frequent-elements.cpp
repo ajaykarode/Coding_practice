fa4757b47770d49ceed8cadbc362283b6c37f3a4
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-06-09 21:06:39 +0530

    Time: 20 ms (50.17%), Space: 13.6 MB (84.61%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,31 +1,63 @@
+// class Solution {
+// public:
+    
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        
+//         unordered_map<int,int>umap;
+//         for(auto i: nums){
+//             umap[i]++;
+//         }
+        
+//         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        
+//         for(auto x: umap){
+//             pq.push(make_pair(x.second,x.first));
+//             if(pq.size() > k){
+//                 pq.pop();
+//             }
+//         }
+//         vector<int>ans;
+//         while(!pq.empty()){
+//             pair<int,int> temp = pq.top();
+//             pq.pop();
+//             ans.push_back(temp.second);
+            
+//         }
+//         return ans;
+        
+  
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
         
-        unordered_map<int,int>umap;
+        unordered_map<int,int>mp;
         for(auto i: nums){
-            umap[i]++;
+            mp[i]++;
         }
-        
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
         
-        for(auto x: umap){
+        for(auto x: mp){
             pq.push(make_pair(x.second,x.first));
-            if(pq.size() > k){
+            if(pq.size()> k){
                 pq.pop();
             }
         }
         vector<int>ans;
         while(!pq.empty()){
-            pair<int,int> temp = pq.top();
+            pair<int,int>temp = pq.top();
             pq.pop();
             ans.push_back(temp.second);
-            
         }
         return ans;
-        
   
     }
 };
