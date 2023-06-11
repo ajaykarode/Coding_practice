bbb79297213ee0681f323ce34eca443100410644
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-06-12 01:15:51 +0530

    Time: 174 ms (89.73%), Space: 77.1 MB (77.11%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,34 +2,54 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         
+//         int n = heights.size();
+//         int leftsmall[n];
+//         int rightsmall[n];
+//         stack<int>st;
+        
+//         for(int i = 0;i<n;i++){
+//             while(!st.empty() && heights[st.top()] >= heights[i]){
+//                 st.pop();
+//             }
+//             if(st.empty()) leftsmall[i] = 0;
+//             else leftsmall[i] = st.top()+1;
+//             st.push(i);
+//         }
+        
+//         while(!st.empty()) st.pop();
+        
+//         for(int i = n-1;i>=0;i--){
+//             while(!st.empty() && heights[st.top()] >= heights[i]){
+//                 st.pop();
+//             }
+//             if(st.empty()) rightsmall[i] = n-1;
+//             else rightsmall[i] = st.top() - 1;
+//             st.push(i);
+//         }
+//         int ar = 0;
+        
+//         for(int i = 0;i<n;i++){
+//             ar = max(ar, heights[i] * (rightsmall[i] - leftsmall[i] + 1));
+//         }
+//         return ar;
+        
+        
         int n = heights.size();
-        int leftsmall[n];
-        int rightsmall[n];
+        int ar = 0;
         stack<int>st;
         
-        for(int i = 0;i<n;i++){
-            while(!st.empty() && heights[st.top()] >= heights[i]){
+        for(int i = 0;i<=n;i++){
+            while(!st.empty() && (i == n  || heights[st.top()] > heights[i])){
+                int height = heights[st.top()];
                 st.pop();
+                int width;
+                if(st.empty()) width = i;
+                else width = i - st.top() - 1;
+                
+                ar = max(ar, width* height);
+                
             }
-            if(st.empty()) leftsmall[i] = 0;
-            else leftsmall[i] = st.top()+1;
             st.push(i);
-        }
-        
-        while(!st.empty()) st.pop();
-        
-        for(int i = n-1;i>=0;i--){
-            while(!st.empty() && heights[st.top()] >= heights[i]){
-                st.pop();
-            }
-            if(st.empty()) rightsmall[i] = n-1;
-            else rightsmall[i] = st.top() - 1;
-            st.push(i);
-        }
-        int ar = 0;
-        
-        for(int i = 0;i<n;i++){
-            ar = max(ar, heights[i] * (rightsmall[i] - leftsmall[i] + 1));
         }
         return ar;
             
