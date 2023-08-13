33a2ee1c31e0aa78ccbf54424c8bd6b84a0a2ff3
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-08-13 23:13:50 +0530

    Time: 3 ms (41.52%), Space: 6.3 MB (77.44%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,22 +1,50 @@
 class Solution {
 public:
-    bool isValid(string s) {
+//     bool isValid(string s) {
+//         stack<char>st;
+//         for(auto it: s){
+//             if(it == '(' || it=='{' || it=='['){
+//                 st.push(it);
+//             }
+//             else{
+//                 if(st.size() == 0) return false;
+//                 char ch = st.top(); 
+//                 st.pop();
+//                 if((it == ')' && ch == '(') || (it == ']' && ch == '[') || (it == '}' && ch == '{')) continue;        
+//                 else return false;
+                
+//             }
+//         }
+//         return st.empty();
+        
+//     }
+    
+    
+    bool isValid(string s){
+        
         stack<char>st;
-        for(auto it: s){
-            if(it == '(' || it=='{' || it=='['){
-                st.push(it);
+        for(auto i: s){
+            if(i == '(' || i == '{' || i == '['){
+                st.push(i);
             }
             else{
-                if(st.size() == 0) return false;
-                char ch = st.top(); 
-                st.pop();
-                if((it == ')' && ch == '(') || (it == ']' && ch == '[') || (it == '}' && ch == '{')) continue;        
-                else return false;
                 
+                if(st.size() == 0) return false;
+                char c = st.top();
+                st.pop();
+                
+                if((c == '(' && i == ')') || (c == '{' && i == '}') || (c == '[' && i == ']')) continue;
+        
+                else return false;
             }
         }
+        
         return st.empty();
+        
+        
         
     }
     
 };
+
+
