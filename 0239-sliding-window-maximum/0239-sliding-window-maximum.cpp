7cce2dfe83afc60c57e39a51f3a11ed14b018365
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-08-22 01:20:36 +0530

    Time: 554 ms (8.03%), Space: 212 MB (6.79%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,28 +1,57 @@
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+// //         deque<int>dq;
+// //         vector<int>ans;
+// //         int n = nums.size();
+        
+// //         for(int i = 0;i<k;i++){
+// //             while(!dq.empty() && nums[dq.back()] < nums[i]){
+// //                 dq.pop_back();
+// //             }
+// //             dq.push_back(i);
+// //         }
+// //         ans.push_back(nums[dq.front()]);
+        
+// //         for(int i = k;i<n;i++){
+// //             if(dq.front() == i-k){
+// //                 dq.pop_front();
+// //             }
+// //             while(!dq.empty() && nums[dq.back()] < nums[i]){
+// //                 dq.pop_back();
+// //             }
+// //             dq.push_back(i);
+// //             ans.push_back(nums[dq.front()]);
+// //         }
+// //         return ans;
+        
+        
+
+//     }
+// };
+
+
+
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int>dq;
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+
+    int n = arr.size();
         vector<int>ans;
-        int n = nums.size();
+    multiset<int,greater<int>>s;
+    for(int i = 0;i<k;i++){
+        s.insert(arr[i]);
+    }
+    ans.push_back(*s.begin());
+
+
+    for(int i = k;i<n;i++){
+        s.erase(s.lower_bound(arr[i-k]));
+        s.insert(arr[i]);
+        ans.push_back(*s.begin());
         
-        for(int i = 0;i<k;i++){
-            while(!dq.empty() && nums[dq.back()] < nums[i]){
-                dq.pop_back();
-            }
-            dq.push_back(i);
-        }
-        ans.push_back(nums[dq.front()]);
-        
-        for(int i = k;i<n;i++){
-            if(dq.front() == i-k){
-                dq.pop_front();
-            }
-            while(!dq.empty() && nums[dq.back()] < nums[i]){
-                dq.pop_back();
-            }
-            dq.push_back(i);
-            ans.push_back(nums[dq.front()]);
-        }
+
+    }
         return ans;
     }
 };
