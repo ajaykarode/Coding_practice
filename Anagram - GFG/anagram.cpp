74bfa17e4b010eba835cbe3961dd754dc64188f5
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-08-22 13:42:45 +0530

    Added solution - LeetHub

--- Anagram - GFG/anagram.cpp
+++ Anagram - GFG/anagram.cpp
@@ -11,21 +11,34 @@ class Solution
     bool isAnagram(string a, string b){
         
         // Your code here
-        int n1 = a.length();
-        int n2 = b.length();
+        // int n1 = a.length();
+        // int n2 = b.length();
         
-        if(n1!= n2) return false;
+        // if(n1!= n2) return false;
         
+        // sort(a.begin(),a.end());
+        // sort(b.begin(),b.end());
+        
+        // for(int i = 0;i<n1;i++){
+        //     if(a[i] != b[i]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        
+        
+        int flag = 0;
         sort(a.begin(),a.end());
         sort(b.begin(),b.end());
         
-        for(int i = 0;i<n1;i++){
-            if(a[i] != b[i]){
-                return false;
-            }
+        if(a.compare(b) == 0){
+            flag = 1;
         }
-        return true;
-        
+        else{
+        flag = 0;
+            
+        }
+        return flag;
     }
 
 };
