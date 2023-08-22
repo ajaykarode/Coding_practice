4a0186d592f259797e39f5d1943698bebd69ba61
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-08-23 00:20:24 +0530

    Time: 3 ms (84.84%), Space: 13.5 MB (63.05%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -14,40 +14,69 @@ public:
     
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         
-        map<int,map<int,multiset<int>>> nodes;
-        queue<pair<TreeNode*,pair<int,int>>> todo;
+//         map<int,map<int,multiset<int>>> nodes;
+//         queue<pair<TreeNode*,pair<int,int>>> todo;
         
-        todo.push({root,{0,0}});
+//         todo.push({root,{0,0}});
         
-        while(!todo.empty()){
-            auto p = todo.front();
-            todo.pop();
+//         while(!todo.empty()){
+//             auto p = todo.front();
+//             todo.pop();
             
-            TreeNode* node =  p.first;
-            int x =  p.second.first;
+//             TreeNode* node =  p.first;
+//             int x =  p.second.first;
+//             int y = p.second.second;
+            
+//             nodes[x][y].insert(node->val);
+            
+//             if(node->left){
+//                 todo.push({node->left,{x-1,y+1}});
+//             }
+//             if(node->right){
+//                 todo.push({node->right,{x+1,y+1}});
+//             }        
+//         }
+//         vector<vector<int>>ans;
+        
+//         for(auto p : nodes){
+//             vector<int>col;
+//             for(auto q: p.second){
+//                 col.insert(col.end(),q.second.begin(),q.second.end());
+//             }
+//             ans.push_back(col);
+//         }
+//         return ans;
+        
+        map<int,map<int,multiset<int>>>mp;
+        queue<pair<TreeNode*,pair<int,int>>>q;
+        q.push({root,{0,0}});
+        
+        while(!q.empty()){
+            auto p = q.front();
+            q.pop();
+            TreeNode* node = p.first;
+            int x = p.second.first;
             int y = p.second.second;
-            
-            nodes[x][y].insert(node->val);
+            mp[x][y].insert(node->val);
             
             if(node->left){
-                todo.push({node->left,{x-1,y+1}});
+                q.push({node->left,{x-1,y+1}});
             }
             if(node->right){
-                todo.push({node->right,{x+1,y+1}});
+                q.push({node->right,{x+1,y+1}});
             }
-        
-            
         }
-        vector<vector<int>>ans;
         
-        for(auto p : nodes){
-            vector<int>col;
-            for(auto q: p.second){
-                col.insert(col.end(),q.second.begin(),q.second.end());
+        vector<vector<int>>ans;
+        for(auto p : mp){
+            vector<int>inl;
+            for(auto q : p.second){
+                inl.insert(inl.end(),q.second.begin(),q.second.end());
             }
-            ans.push_back(col);
+            ans.push_back(inl);
         }
         return ans;
         
     }
 };
+
