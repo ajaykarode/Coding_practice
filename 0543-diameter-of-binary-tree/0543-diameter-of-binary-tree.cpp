c6b79842002fc7b095f3ed07bf7dd09b13d36510
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-08-22 22:08:39 +0530

    Time: 0 ms (100.00%), Space: 20.1 MB (98.53%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -9,22 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ 
+// class Solution {
+// public:
+    
+//     int height(TreeNode* root, int& diameter){
+//         if(root == NULL) return 0;
+        
+//         int lh = height(root->left,diameter);
+//         int rh = height(root->right,diameter);
+//         diameter = max(diameter, lh+rh);
+//         return 1+ max(lh,rh);
+//     }
+
+//     int diameterOfBinaryTree(TreeNode* root) {
+        
+//         int diameter = 0;
+//         height(root,diameter);
+//         return diameter;
+//     }
+// };
+
 class Solution {
 public:
     
-    int height(TreeNode* root, int& diameter){
-        if(root == NULL) return 0;
-        
-        int lh = height(root->left,diameter);
-        int rh = height(root->right,diameter);
-        diameter = max(diameter, lh+rh);
+    int height(TreeNode* root, int& d){
+        if(root == NULL){
+            return 0;
+        }
+        int lh = height(root->left,d);
+        int rh = height(root->right,d);
+        d = max(d,lh+rh);
         return 1+ max(lh,rh);
+        
     }
 
     int diameterOfBinaryTree(TreeNode* root) {
-        
-        int diameter = 0;
-        height(root,diameter);
-        return diameter;
+       
+        int d = 0;
+        height(root,d);
+        return d;
     }
 };
