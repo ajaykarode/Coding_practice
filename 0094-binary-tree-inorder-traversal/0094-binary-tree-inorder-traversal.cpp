3190ef1cbab3a9b53ad192a0b821546d14182ac5
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-08-22 22:18:54 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (24.03%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,26 +9,67 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+    
+//     void inorder(TreeNode* root,vector<int>&ans){
+//         if(root == NULL){
+//             return;
+//         }       
+//         inorder(root->left,ans);
+//         ans.push_back(root->val);
+//         inorder(root->right,ans);
+//     }
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int>ans;
+//         if(root == NULL){
+//             return ans;
+//         }
+//         inorder(root,ans);
+        
+//         return ans;
+        
+//     }
+// };
+
+
 class Solution {
 public:
     
-    void inorder(TreeNode* root,vector<int>&ans){
-        if(root == NULL){
-            return;
-        }
-        
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-    }
+    // 
+    
+    
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        if(root == NULL){
-            return ans;
-        }
-        inorder(root,ans);
         
+        TreeNode* curr = root;
+        vector<int>ans;
+        
+        while(curr!= NULL){
+            if(curr->left == NULL){
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+                TreeNode* temp = curr->left;
+                while(temp->right != NULL && temp->right != curr){
+                    temp = temp->right;
+                }
+                if(temp->right == NULL){
+                    temp->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    temp->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+            
+        }
         return ans;
+        
+ 
         
     }
 };
+
