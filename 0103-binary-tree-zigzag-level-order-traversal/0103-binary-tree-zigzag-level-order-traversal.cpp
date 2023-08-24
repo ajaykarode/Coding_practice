b2c1f85d458614236130b7e3c1b6e7c36bb8f400
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-08-24 19:20:56 +0530

    Time: 5 ms (29.91%), Space: 12.2 MB (25.75%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
@@ -29,9 +30,54 @@ public:
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
             }
-            lefttoright = !lefttoright;
+            lefttoright = !lefttoright; 
             ans.push_back(temp);
         }
         return ans;
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         vector<vector<int>>ans;
+//         queue<TreeNode*> q;
+//         q.push(root);
+//         bool lefttoright = true;
+        
+//         while(!q.empty()){
+//             int size = q.size();
+//             vector<int>row(size);
+            
+//             for(int i = 0;i<size;i++){
+//                 TreeNode* temp = q.front();
+//                 q.pop();
+//                 int index = lefttoright ? i : (size - 1 - i);
+//                 row[index] = temp->val;
+//                 if(temp->left){
+//                     q.push(temp->left);
+//                 }
+//                 if(temp->right){
+//                     q.push(temp->right);
+//                 }
+//             }
+            
+//             lefttoright = !lefttoright;
+//             ans.push_back(row);
+            
+            
+//         }
+//         return ans;
     }
+    
 };
