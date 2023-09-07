7f5242de3491e2b1722cf1c7c8f55549c6dd6b45
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-09-08 01:07:32 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (99.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -10,3 +10,23 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+
+
+// this sol gives tle
+// class Solution {
+// public:
+    
+//     int unique(int i,int j,int m,int n){
+//         if(i == m-1 && j == n-1) return 1;
+//         if(i > m-1 || j > n-1) return 0;
+//         else return unique(i+1,j,m,n) + unique(i,j+1,m,n);
+//     }
+//     int uniquePaths(int m, int n) {
+//         return unique(0,0,m,n);
+//     }
+// };
