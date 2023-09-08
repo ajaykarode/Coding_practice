d01c3d4ae25bf8f71bb2509882fca9b653b20a2c
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-09-09 00:00:17 +0530

    Time: 2 ms (46.16%), Space: 6 MB (48.16%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==1 || n==2){
+            return 1;
+        }
+        if(n==0){
+            return 0;
+        }
+        int one = 0  , two = 1 , three = 1 , val = 0;
+        for(int i = 3 ; i <= n ; i++){
+            val = one + two + three;
+            one = two;
+            two = three;
+            three = val;
+        }
+        return val;
+    }
+};
+
+
+
+    
+
+
+
+// if(n == 0 || n == 1 || n == 2) return n;
+        
+//         int f = 0,s = 1,t = 2,ans = 0;
+//         for(int i = 3;i<=n;i++){
+//             ans = f+s+t;
+//             f = s;
+//             s = t;
+//             t = ans;
+//         }
+//         return ans;
