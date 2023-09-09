2a8549cb99ac1eb1b964dd919c09d5663b36ec74
AJAY KARODE <92324991+ajaykarode@users.noreply.github.com>
2023-09-09 22:50:29 +0530

    Time: 4 ms (53.29%), Space: 7.4 MB (94.71%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,39 +1,62 @@
-class Solution {
-public:
-    void func(int ind,vector<int>&nums,vector<int>&ds,vector<vector<int>>&ans){
-        // ans.push_back(ds);
-        // for (int i = ind; i < nums.size(); i++) {
-        //     if (i != ind && nums[i] == nums[i - 1]) continue;
-        //     ds.push_back(nums[i]);
-        //     func(i + 1, nums, ds, ans);
-        //     ds.pop_back();
-        //  }
-        
-        ans.push_back(ds);
-        for(int i = ind;i<nums.size();i++){
-            if(i != ind && nums[i] == nums[i-1]) continue;
-            ds.push_back(nums[i]);
-            func(i+1,nums,ds,ans);
-            ds.pop_back();
-        }
+// class Solution {
+// public:
+//     void func(int ind,vector<int>&nums,vector<int>&ds,vector<vector<int>>&ans){
+
+//         ans.push_back(ds);
+//         for(int i = ind;i<nums.size();i++){
+//             if(i != ind && nums[i] == nums[i-1]) continue;
+//             ds.push_back(nums[i]);
+//             func(i+1,nums,ds,ans);
+//             ds.pop_back();
+//         }
     
-    }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//     }
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         
-   
-//         vector<vector<int>>ans;
-//         vector<int>ds;
+
         
 //         sort(nums.begin(),nums.end());
+//         vector<vector<int>>ans;
+//         vector<int>ds;
 //         func(0,nums,ds,ans);
 //         return ans;
+            
+        
+        
+//     }
+// };
+
+
+
+class Solution {
+public:
+    void func(int ind,vector<int>&nums,vector<int>&temp,vector<vector<int>>&ans){
+  
+        
+        
+        ans.push_back(temp);
+        for(int i = ind;i<nums.size();i++){
+            if(i != ind && nums[i] == nums[i-1]) continue;
+            temp.push_back(nums[i]);
+            func(i+1,nums,temp,ans);
+            temp.pop_back();
+        }
+        
+    
+    
+    }
+    
+    
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
-        vector<int>ds;
-        func(0,nums,ds,ans);
+        vector<int>temp;
+        func(0,nums,temp,ans);
         return ans;
-            
+        
+
         
         
     }
